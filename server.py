558748f21ee931d4026df32d96ed2555b61bf023
Raman Sarda <theloudspeaker@disroot.org>
2021-04-13 04:27:50 +0530

    Add feature: saving of stream

--- server.py
+++ server.py
@@ -3,10 +3,19 @@ import socket
 import struct
 from PIL import Image
 import matplotlib.pyplot as pl
+from subprocess import Popen, PIPE
+import datetime
 
 server_socket = socket.socket()
 server_socket.bind(('0.0.0.0', 8000))  #Here you can change the IP and port
 server_socket.listen(0)
+
+# naming the outputfile
+startdate=datetime.datetime.now()
+startdate=datetime.datetime.strftime(startdate,"%H%M%S-%d%m%Y")
+videofile=startdate+".avi"
+# Writing to video file using ffmpeg.
+p = Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-vcodec', 'mjpeg', '-r', '2', '-i', '-', '-vcodec', 'mpeg4', '-q:v', '1', '-r', '2', videofile], stdin=PIPE)
 
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('rb')
@@ -26,7 +35,7 @@ try:
         # processing on it
         image_stream.seek(0)
         image = Image.open(image_stream)
-        
+        image.save(p.stdin, 'JPEG')
         if img is None:
             img = pl.imshow(image)
         else:
@@ -34,7 +43,6 @@ try:
 
         pl.pause(0.01)
         pl.draw()
-
         print('Image is %dx%d' % image.size)
         image.verify()
         print('Image is verified')
